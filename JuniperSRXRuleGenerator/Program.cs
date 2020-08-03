651a41f07849b70febb36462ffeabf767b3517a6
Vishno Krishnarajah <k.vishno@gmail.com>
2020-08-03 11:20:24 +0200

    Add project files.

---
+++ JuniperSRXRuleGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JuniperSRXRuleGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
